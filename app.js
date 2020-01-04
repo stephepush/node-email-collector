6e45ec3e17b75b509745d44e9ef577ed61595967
Stephen Peters <7speter@gmail.com>
2020-01-03 22:54:43 -0500

    Adds ejs index template, and partials

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ app.use(bodyParser.urlencoded({extended: false}))
 //app.use(express.static('./public'));
 
 app.use(morgan('short'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    //res.send("Hello from your local node server")
+    res.render('pages/index')
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is up and listening on port 3000")
+})
