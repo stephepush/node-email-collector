a1e26b31ac4b74e2b8b0c7f745aa4255a99ac98b
Stephen Peters <7speter@gmail.com>
2020-01-05 20:27:42 -0500

    Adds pg pool object to subscribe.js

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'email-collector',
+    password: '_6jrF4U&6nuYe>&*SWJbT:3s!:)WeiL~k^b^4b+a[yRD!',
+    port: 5432,
+})
+
 router.get('/', (req, res)=>{
     res.render('pages/subscribe')
 });
