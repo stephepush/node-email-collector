5477f952ad84354983e7335aa2cec4a9b75c6aca
Stephen Peters <7speter@gmail.com>
2020-01-08 20:51:59 -0500

    Adds SELECT statement to index.js route to show how many users signed up on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+var db = require('../db');
 
 router.use('/subscribe', require('./subscribe.js'));
 router.use('/about', require('./about.js'));
+let userCount;
+
+db.query(`SELECT count(*) AS exact_count from users`, (error,results) => {
+    if (error) {
+        console.log(error.stack)
+    } else {
+        return userCount = results.rows[0]['exact_count']
+        console.log(userCount)}
+})
 
 router.get('/', (req, res)=>{
     //res.send("Hello from your local node server")
-    res.render('pages/index')
+    userCount
+    res.render('pages/index', {userCount:userCount})
 });
 
 module.exports = router;
