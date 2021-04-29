797b27e645c3ebc7bd61455dd1ed0d2c26555184
stephepush <7speter@gmail.com>
2021-04-29 13:06:48 -0400

    Attempt to get pg credentials working on heroku

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
+    //user: process.env.DATABASE_USER,
     host: process.env.DATABASE_URL,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DB_KEY,
-    port: process.env.DB_PORT
+    //database: process.env.DATABASE_NAME,
+    p//assword: process.env.DB_KEY,
+    //port: process.env.DB_PORT
 })
 
 
