8abc72197f91dd13411508d2cc0edb4713a77964
stephepush <7speter@gmail.com>
2021-05-04 10:19:12 -0400

    Adjusts index view controller/route

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,20 @@ router.use('/subscribe', require('./subscribe.js'));
 router.use('/about', require('./about.js'));
 let userCount;
 
-db.query(`SELECT count(*) AS exact_count from users`, (error,results) => {
-    if (error) {
-        console.log(error.stack)
-    } else {
-        return userCount = results.rows[0]['exact_count']
-        console.log(userCount)}
-})
+
 
 router.get('/', (req, res)=>{
     //res.send("Hello from your local node server")
+    db.query(`SELECT count(*) AS exact_count from users`, (error,results) => {
+        if (error) {
+            //console.log(error.stack)
+        } /* else {
+            return userCount = results.rows[0]['exact_count']
+            //console.log(userCount)
+        } */
+        res.render('pages/index', {userCount:results.rows[0]['exact_count']});
+    });
     
-    res.render('pages/index', {userCount:userCount})
 });
 
 module.exports = router;
