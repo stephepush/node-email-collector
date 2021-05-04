86193d2c72df274434252778da7e804bc3c1935b
stephepush <7speter@gmail.com>
2021-05-04 10:40:31 -0400

    Reverts index view controller/route

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,19 @@ router.use('/subscribe', require('./subscribe.js'));
 router.use('/about', require('./about.js'));
 let userCount;
 
-
+db.query(`SELECT count(*) AS exact_count from users`, (error,results) => {
+    if (error) {
+        //console.log(error.stack)
+    } else {
+        return userCount = results.rows[0]['exact_count']
+        //console.log(userCount)
+    }
+})
 
 router.get('/', (req, res)=>{
     //res.send("Hello from your local node server")
-    db.query(`SELECT count(*) AS exact_count from users`, (error,results) => {
-        if (error) {
-            //console.log(error.stack)
-            return res.render('pages/error', { error });
-        } /* else {
-            return userCount = results.rows[0]['exact_count']
-            //console.log(userCount)
-        } */
-        res.render('pages/index', {userCount:results.rows[0]['exact_count']});
-    });
     
+    res.render('pages/index', {userCount:results.rows[0]['exact_count']});
 });
 
 module.exports = router;
